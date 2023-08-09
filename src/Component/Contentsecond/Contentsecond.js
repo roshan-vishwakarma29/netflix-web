f5708301dd1d9d9522a49ef9219adb0878e39c25
roshan-vishwakarma29 <roshanredminote9@gmail.com>
2023-08-09 20:23:42 +0530

    added

--- src/Component/Contentsecond/Contentsecond.js
+++ src/Component/Contentsecond/Contentsecond.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Contentsecod.css'
 import mobileimg from '../../Assert/images/mobile-0819.jpg'
+import stranger from '../../Assert/images/stranger.png'
+import icon from '../../Assert/images/icon.gif'
 
 const Contentsecond = () => {
     return (
@@ -9,6 +11,19 @@ const Contentsecond = () => {
                 <div className='watch_box'>
                     <div className='mobile'>
                         <img className='mobile_images' src={mobileimg} alt="" />
+                        <div className='overlap_mobile'>
+                            <div>
+                                <img src={stranger} style={{ height: "100px" }} alt="" />
+                                <div>
+                                    <p className='Things'>Stranger <br /> Things <br />
+                                        <span>Downloading...</span></p>
+                                </div>  
+                            </div>
+                            <div>
+                                <img src={icon} s alt="" />
+                            </div>
+
+                        </div>
                     </div>
                     <div className='easily'>
                         <h1 className='favourites'>Download your shows to watch offline</h1>
